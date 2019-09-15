68c34c32d8e9435376e9e009bc32548ac8c8d083
tamtan <vinhtam92@gmail.com>
2019-09-15 11:56:19 -0500

    - init

---
+++ BasicActivity/app/src/main/java/vinh/tan/tam/com/basicactivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package vinh.tan.tam.com.basicactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
